4e5f9887f7fac2f2e7900ba15d52a89a0ffb43f6
Jeff-desv <brunojeff0012@gmail.com>
2024-06-05 20:40:26 -0300

    Teste

--- desfioPoo/src/br/com/dio/deafio/dominio/Curso.java
+++ desfioPoo/src/br/com/dio/deafio/dominio/Curso.java
@@ -1,28 +1,18 @@
 package br.com.dio.deafio.dominio;
 
-public class Curso {
+public class Curso extends Connteudo {
 
-	private String titulo;
-	private String descricao;
 	private int cargaHoraria;
 	
 	public Curso() {
 		
 	}
 	
+	@Override
+	public double calcularXp() {
+		return XP_PADDRAO * cargaHoraria;
+	}
 	
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
 	public int getCargaHoraria() {
 		return cargaHoraria;
 	}
@@ -31,8 +21,8 @@ public class Curso {
 	}
 	@Override
 	public String toString() {
-		return "Curso [titulo = " + titulo + ", descricao = " + descricao + ", cargaHoraria = " + cargaHoraria + "]";
+		return "Curso [titulo = " + getTitulo() + ", descricao = " + getDescricao() + ", cargaHoraria = " + cargaHoraria + "]";
 	}
-	
+
 	
 }
