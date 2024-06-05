5565060bb76a6ef951f278729ab83757746b1a0d
Jeff-desv <brunojeff0012@gmail.com>
2024-06-04 21:23:06 -0300

    Testando os metodos

--- desfioPoo/src/desfioPoo/Main.java
+++ desfioPoo/src/desfioPoo/Main.java
@@ -1,9 +1,38 @@
 package desfioPoo;
 
+import java.time.LocalDate;
+
+import br.com.dio.deafio.dominio.Curso;
+import br.com.dio.deafio.dominio.Mentoria;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Curso curso1 = new Curso();
+		
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("Descrição Curso Java");
+		curso1.setCargaHoraria(10);
+		
+		Curso curso2 = new Curso();
+		
+		curso2.setTitulo("Curso Javascript");
+		curso2.setDescricao("Descrição Curso Javascript");
+		curso2.setCargaHoraria(8);
+		
+		System.out.println();
+		System.out.println(curso1);
+		System.out.println();
+		System.out.println(curso2);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria Java");
+		mentoria.setDescricao("Descrição mentoria Java");
+		mentoria.setData(LocalDate.now());
+		
+		System.out.println();
+		System.out.println(mentoria);
 
 	}
 
