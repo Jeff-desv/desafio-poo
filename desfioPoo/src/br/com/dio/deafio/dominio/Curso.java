f8f17eefe4a2ec3a9b20a38bdbd75599ffdf731d
Jeff-desv <brunojeff0012@gmail.com>
2024-06-04 21:02:40 -0300

    Criando Classes

--- desfioPoo/src/br/com/dio/deafio/dominio/Curso.java
+++ desfioPoo/src/br/com/dio/deafio/dominio/Curso.java
@@ -2,5 +2,7 @@ package br.com.dio.deafio.dominio;
 
 public class Curso {
 
-	
+	String titulo;
+	String descricao;
+	int cargaHoraria;
 }
--- desfioPoo/src/br/com/dio/deafio/dominio/Mentoria.java
+++ desfioPoo/src/br/com/dio/deafio/dominio/Mentoria.java
@@ -1,5 +1,10 @@
 package br.com.dio.deafio.dominio;
 
+import java.time.LocalDate;
+
 public class Mentoria {
 
+	String titulo;
+	String descricao;
+	LocalDate data;
 }
